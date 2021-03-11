9d772646997d133dbc36ebc2c0896e8594bbfec5
Lityuk <lityuk@gmail.com>
2021-03-11 17:20:40 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        let currentValue = array[0];
+        array.forEach(function (item, index) {
+            if (currentValue > array[index]) {
+                currentValue = array[index];
+            }
+        });
+        return currentValue;
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        let currentValue = array[0];
+        array.forEach(function (item, index) {
+          if (currentValue < array[index]) {
+            currentValue = array[index];
+          }
+        });
+        return currentValue;
+      }
+      return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return array.reduce(function(previousValue, currentValue){
+            previousValue + currentValue}) / array.length;
+      } else {
+        return 0;
+      }
+    };
